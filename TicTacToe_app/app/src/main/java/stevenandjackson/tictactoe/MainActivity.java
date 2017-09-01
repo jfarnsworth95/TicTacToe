d25390c5974ee06cf36f5e3594afedac00e3f0f0
Jackson F <jafarns95@gmail.com>
2017-08-31 20:36:23 -0500

    new and improved

--- TicTacToe_app/app/src/main/java/stevenandjackson/tictactoe/MainActivity.java
+++ TicTacToe_app/app/src/main/java/stevenandjackson/tictactoe/MainActivity.java
@@ -1,11 +1,16 @@
 package stevenandjackson.tictactoe;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,20 +28,33 @@ public class MainActivity extends AppCompatActivity {
 
     static final int CROSS = 1, CIRCLE =2;
 
+    ArrayList<Button> btnLst;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnLst = new ArrayList<Button>();
+
         tl = (Button) findViewById(R.id.topLeft);
+        btnLst.add(tl);
         tm = (Button) findViewById(R.id.topMid);
+        btnLst.add(tm);
         tr = (Button) findViewById(R.id.topRight);
+        btnLst.add(tr);
         ml = (Button) findViewById(R.id.midLeft);
+        btnLst.add(ml);
         m = (Button) findViewById(R.id.mid);
+        btnLst.add(m);
         mr = (Button) findViewById(R.id.midRight);
+        btnLst.add(mr);
         bl = (Button) findViewById(R.id.botLeft);
+        btnLst.add(bl);
         bm = (Button) findViewById(R.id.botMid);
+        btnLst.add(bm);
         br = (Button) findViewById(R.id.botRight);
+        btnLst.add(br);
 
         gc = new GameCore();
 
@@ -163,12 +181,57 @@ public class MainActivity extends AppCompatActivity {
 
         setButtons();
 
+        gc.gameStatus = GameCore.inspectGameStatus(gc.board);
+
         if (gc.gameStatus == GameCore.AI_WON) {
             Toast.makeText(MainActivity.this, "You Lost", Toast.LENGTH_LONG).show();
+            requestReset();
         } else if (gc.gameStatus == GameCore.PLAYER_WON) {
             Toast.makeText(MainActivity.this, "You Won", Toast.LENGTH_LONG).show();
+            requestReset();
         } else if (gc.gameStatus == GameCore.DRAW) {
             Toast.makeText(MainActivity.this, "Draw", Toast.LENGTH_LONG).show();
+            requestReset();
+        }
+    }
+
+    public void requestReset(){
+        Log.i("Request", "About to send request to restart");
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.i("Request", "AlertDialog being built");
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                // set title
+                alertDialogBuilder.setTitle("New Game?");
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("Restart",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                reset();
+                            }
+                        })
+                        .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // if this button is clicked, just close the dialog box and do nothing
+                                finishAndRemoveTask();
+                            }
+                        });
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                // show it
+                alertDialog.show();
+            }
+        }, 3000);
+    }
+
+    public void reset(){
+        gc = new GameCore();
+        for(int i = 0; i < btnLst.size(); i++){
+            btnLst.get(i).setText("");
+            btnLst.get(i).setEnabled(true);
         }
     }
 }
